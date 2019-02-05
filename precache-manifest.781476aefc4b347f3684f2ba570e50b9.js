80a83f812242f69f942f082422ce30aff85dcd02
natalia <natalia.verstka@yandex.ru>
2019-02-05 12:52:08 +0300

    build

---
+++ precache-manifest.781476aefc4b347f3684f2ba570e50b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5fc7afe78abf3aaefeb",
+    "url": "/static/js/main.f5fc7afe.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "f5fc7afe78abf3aaefeb",
+    "url": "/static/css/main.602f894b.chunk.css"
+  },
+  {
+    "revision": "e2260408d57de11f3f863e5e07393cf2",
+    "url": "/index.html"
+  }
+];
